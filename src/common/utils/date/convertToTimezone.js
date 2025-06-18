e12a5a5e694263a92c7bde15b0f5212b1b8d611e
Roshan <dearjha2019@gmail.com>
2025-06-18 17:13:49 +0530

    changes

--- src/common/utils/date/convertToTimezone.js
+++ src/common/utils/date/convertToTimezone.js
@@ -1,13 +1,3 @@
 export const convertToTimezone = (date, timezone) => {
-  const localDate = new Date(date).toLocaleString('en-US', { timeZone: timezone });
-  const d = new Date(localDate);
-
-  const day = `${d.getDate()}`.padStart(2, '0');
-  const month = `${d.getMonth() + 1}`.padStart(2, '0');
-  const year = d.getFullYear();
-  const hours = `${d.getHours()}`.padStart(2, '0');
-  const minutes = `${d.getMinutes()}`.padStart(2, '0');
-  const seconds = `${d.getSeconds()}`.padStart(2, '0');
-
-  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  return new Date(date).toLocaleString('en-US', { timeZone: timezone });
 };
