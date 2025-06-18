63201c15a8566ae98786efcaa30ae39751da1af1
Abhilasha Singh <abhilasha3021@gmail.com>
2025-06-18 13:18:35 +0530

    Completed the task of error handling middleware

--- src/common/routes/publicRoutes.js
+++ src/common/routes/publicRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 import authRouter from "../../modules/User/v1/routes/authRoutes.js";
 import companyRoutes from '../../modules/User/v1/routes/companyRoutes.js';
 import { validatePhoneNumber } from '../utils/validation/validatePhone.js';
+import { errorHandler } from '../middlewares/errorHandler.js';
 
 const publicRouter = express.Router(); 
 
 publicRouter.use('/v1/auth', authRouter);              
-publicRouter.use('/v1/company', companyRoutes);        
+publicRouter.use('/v1/company', companyRoutes);      
+publicRouter.use(errorHandler);
 
 export default publicRouter;
